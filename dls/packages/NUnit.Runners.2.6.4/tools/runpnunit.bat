7cccfb40d2d52514a08aea60c6a1a5dd02fc83c1
Chris King <kingces95@gmail.com>
2016-09-28 08:50:33 -0700

    add tools dirs

---
+++ dls/packages/NUnit.Runners.2.6.4/tools/runpnunit.bat
@@ -0,0 +1,6 @@
+version https://git-lfx.github.com/spec/v1
+hint NUnit.Runners.2.6.4/tools/runpnunit.bat
+oid sha256:78a2d37826ad9be173977f9e7bdbb698e1eda8488b3e102d790256c3cda7bfe0
+size 81
+type archive
+url http://nuget.org/api/v2/package/NUnit.Runners/2.6.4
