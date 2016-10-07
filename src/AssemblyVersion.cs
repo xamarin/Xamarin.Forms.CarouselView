e543240253c568ab1c69b30c7f823eea511bb6d9
Chris King <kingces95@gmail.com>
2016-10-07 14:16:54 -0700

    NugetReferenceAttribute

--- src/AssemblyVersion.cs
+++
@@ -1,3 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion(BuildInfo.MajorVersion + "." + BuildInfo.MinorVersion + "." + BuildInfo.Number)]
--- src/BuildInfo.cs
+++ src/BuildInfo.cs
@@ -1,5 +1,26 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion(BuildInfo.MajorVersion + "." + BuildInfo.MinorVersion + "." + BuildInfo.Number)]
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+internal class NugetReferenceAttribute : Attribute {
+
+    internal NugetReferenceAttribute(string package, string version, string targetFramework, string assembly) {
+        TargetFramework = targetFramework;
+        Package = package;
+        Version = version;
+        Assembly = assembly;
+    }
+
+    public string Assembly { get; private set; }
+    public string Package { get; private set; }
+    public string Version { get; private set; }
+    public string TargetFramework { get; private set; }
+}
+
 internal class BuildInfo {
-    internal const string Number = "0";
+    internal const string Number = "00";
     internal const string EnlistmentRevision = "";
     internal const string EnlistmentUrl = "";
     internal const string EnslitmentBranch = "";
